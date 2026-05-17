5aa8211f5c8dfb7915c2d31caa738a50b4a4dda3
BalajiMano003 <balajitesting10@gmail.com>
2026-05-17 13:31:43 +0530

    code pushing to feature 1

--- src/test/java/G/Samnple.java
+++ src/test/java/G/Samnple.java
@@ -3,8 +3,12 @@ package G;
 public class Samnple {
 
 	public static void main(String[] args) {
-		// 			Auto-generated method stub create a code for auto valiidate an array
-		afasafafafsfadadsv
+		// 
+
+		
+		Auto-generated method stub create a code for auto valiidate an array
+		
+		System.out.println("Pushing something to branch file");
 	}
 
 }
